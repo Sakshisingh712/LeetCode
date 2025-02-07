facd58b07959cd45af45d1dc3e3027123f3bafe3
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-08 01:06:54 +0530

    Time: 13 ms (19.92%) | Memory: 18.5 MB (12.59%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,15 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # numofZero = flowerbed.count(0)
-        # pos=0
-        # index  = []
         num=len(flowerbed)
         i=0
         if num<=1:
             if n==0:
                 return True
             else:
-                if n==1 and flowerbed[0]==0:
+                if flowerbed[0]==0:
                     return True
                 return False
         while i <=num-1:
@@ -32,68 +29,4 @@ class Solution:
         if n==0:
             return True
         return False
-        # if num<=1:
-        #     if n==0:
-        #         return True
-        #     elif n==1 and flowerbed[0]==0:
-        #         return True
-        #     return False
-        # elif all(s==0 for s in flowerbed):
-        #     if num%2==0:
-        #         if n<= num//2:
-        #             return True
-        #         return False
-        #     else:
-        #         if n<= num//2+1:
-        #             return True
-        #         return False
-        # else:
-        #     if flowerbed[0] == 0:
-        #         i=2
-        #         if flowerbed[1]!=1:
-        #             index.append(0)
-        #             pos += 1
-        #         while i<=num-1:
-        #                 # print(i)
-        #                 if i==num-1:
-        #                     # print('I', i)
-        #                     if flowerbed[i-1]!=1 and flowerbed[i]!=1:
-        #                         if num%2!=0 and i-2 not in index:
-        #                         # print('i',i)
-        #                             index.append(i)
-        #                             pos+=1
-        #                             i+=2
-        #                         elif num%2==0:
-        #                             index.append(i)
-        #                             pos+=1
-        #                             i+=2
-        #                     else:
-        #                         i+=1
-        #                 elif flowerbed[i-1]!=1 and flowerbed[i+1]!=1 and flowerbed[i]!=1:
-        #                     # print('i',i)
-        #                     index.append(i)
-        #                     pos += 1
-        #                     i += 2
-        #                     # print('i',i)
-        #                 else:
-        #                     i+=1
-        #             # print(i, pos)
-        #     elif flowerbed[0] == 1:
-        #         i=2
-        #         while i<=num-1:
-        #                 # print(i)
-        #                 if i==num-1:
-        #                     # print(index)
-        #                     if flowerbed[i-1]!=1 and flowerbed[i]!=1:
-        #                         index.append(i)
-        #                         pos+=1
-        #                 elif flowerbed[i-1]!=1 and flowerbed[i+1]!=1 and flowerbed[i]!=1:
-        #                     index.append(i)
-        #                     pos += 1
-        #                 i+=2  
-        #         # print(pos)  
-        #     if pos>=n:
-        #         return True
-        #     return False
-
-
+        
