3d115db2f2898660dae8b28d9da6052f6dbc4a77
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-12 23:36:38 +0530

    Time: 486 ms (94.64%) | Memory: 29.9 MB (62.19%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -9,12 +9,12 @@ class Solution:
         while i<j:
             if nums[j]>k or nums[i] + nums[j] > k:
                 j-=1
-            elif nums[i]>k or nums[i] + nums[j] < k:
-                i+=1
             elif nums[i]+nums[j] == k:
                 count+=1
                 i+=1
                 j-=1
+            else:
+                i+=1
             # if nums[i] + nums[j] == k:
             #     count+=1
             #     i+=1
