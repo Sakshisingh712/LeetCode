d8e04b46e22faa30c4f1f719bf6111f953a33953
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-13 00:33:51 +0530

    Time: 5 ms (66.91%) | Memory: 18.9 MB (20.40%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,26 +1,17 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # summation = sum(nums)
-        # prefixsum = 0
-        for i in range(len(nums)):
-            if i==0 and sum(nums[i+1:])==0:
-                return i
-            elif i==len(nums)-1 and sum(nums[:-1])==0:
-                print(i)
-                return i
-            elif sum(nums[i+1:]) == sum(nums[:i]):
-                return i
-        return -1
+        summation = sum(nums)
+        prefixsum = 0
         # for i in range(len(nums)):
         #     summation = summation + nums[i]
-        # for j in range(len(nums)):
-        #     if summation - nums[j] == 0 and j==0 :
-        #         return  0
-        #     elif prefixsum == (summation - nums[j]):
-        #         return j
-        #     elif prefixsum == 0 and j==len(nums)-1:
-        #         return j
-        #     else:
-        #         prefixsum += nums[j]
-        #         summation -= nums[j]
-        # return -1
+        for j in range(len(nums)):
+            if summation - nums[j] == 0 and j==0 :
+                return  0
+            elif prefixsum == (summation - nums[j]):
+                return j
+            elif prefixsum == 0 and j==len(nums)-1:
+                return j
+            else:
+                prefixsum += nums[j]
+                summation -= nums[j]
+        return -1
