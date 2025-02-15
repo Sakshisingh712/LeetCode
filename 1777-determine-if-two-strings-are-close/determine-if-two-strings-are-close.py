722fa52909892ecee479164cbc7ea6d85db18f08
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-15 22:37:25 +0530

    Time: 43 ms (97.08%) | Memory: 18 MB (91.40%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,15 +5,12 @@ class Solution:
             return False
         else:
             list1, list2 = [], []
-            # word1, word2 = sorted(word1),
             set1 = list(set(word1))
             set2 = list(set(word2))
             if set(word1)==set(word2):
                 for i in range(len(set1)):
                     list1.append(word1.count(set1[i]))
-                    # print(word1[i], list1)
                     list2.append(word2.count(set2[i]))
-                    # print(word2[i], list2)
                 if sorted(list1) == sorted(list2):
                     return True
             return False
