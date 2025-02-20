53eb5f940968c1535740b02a2db26335445a9f69
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-20 23:16:14 +0530

    Time: 7 ms (59.63%) | Memory: 18.7 MB (86.16%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,3 +1,37 @@
+# class Solution:
+#     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+#         j = 0
+#         n = len(asteroids)
+#         stack = []
+#         # print(not stack)
+#         for i in range(n):
+#             if not stack:
+#                 stack.append(asteroids[i])   
+#                 continue 
+#             if stack[-1]>0:
+#                 if asteroids[i]<0:
+#                     # if abs(stack[-1])>abs(asteroids[i]):
+#                     #     break
+#                     if abs(stack[-1])==abs(asteroids[i]):
+#                         stack.pop()
+#                     elif abs(stack[-1])<abs(asteroids[i]):
+#                         last_pop = 0
+#                         while stack and abs(stack[-1])<=abs(asteroids[i]) and stack[-1]>0 and asteroids[i]<0:
+#                             last_pop = stack[-1]
+#                             stack.pop()
+#                             if last_pop==abs(asteroids[i]):
+#                                 break
+#                         if stack and stack[-1]<0 and abs(last_pop)<abs(asteroids[i]):
+#                             stack.append(asteroids[i])
+#                             # print(asteroids[i], '  ', last_pop)
+#                         if not stack:
+#                             stack.append(asteroids[i])
+#                 else:
+#                     stack.append(asteroids[i])
+#             elif stack[-1]<0:
+#                 stack.append(asteroids[i])
+#                 print(stack)
+#         return stack
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         j = 0
