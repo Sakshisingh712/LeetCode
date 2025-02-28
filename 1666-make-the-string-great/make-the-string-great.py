cda874bb0cfa68e8e3b523e2bb669ed003005ac9
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-02-28 23:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.38%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,13 +1,8 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
-        # print(chr('s'))
+        #chr and ord is the function used to get ascii value.
         for i in s:
-        #     if stack and abs(ord(i)-ord(stack[-1])) == 32:
-        #         stack.pop()
-        #     else:
-        #         stack.append(i)
-        # print(stack)
             if stack and i.islower() and stack[-1].isupper() and stack[-1].lower() == i:
                 stack.pop()
             elif stack and i.isupper() and stack[-1].islower() and stack[-1].upper() == i:
