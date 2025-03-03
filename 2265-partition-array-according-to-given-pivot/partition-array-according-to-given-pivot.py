f0bd521894630b37d9060ac25d14cd90867d144f
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-03-03 22:42:47 +0530

    Time: 36 ms (46.97%) | Memory: 34.4 MB (83.89%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,39 +1,39 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        # list1 = []
-        # list2 = []
-        # equal = []
-        # ind = nums.index(pivot)
+        list1 = []
+        list2 = []
+        equal = []
+        ind = nums.index(pivot)
+        for num in nums:
+            if num<pivot:
+                list1.append(num)
+            elif num>pivot:
+                list2.append(num)
+            else:
+                equal.append(num)
+        list1.extend(equal)
+        list1.extend(list2)   
+        return list1
+        # equal = 0
+        # numLess = 0
+        # ans = [0]*len(nums)
         # for num in nums:
-        #     if num<pivot:
-        #         list1.append(num)
-        #     elif num>pivot:
-        #         list2.append(num)
-        #     else:
-        #         equal.append(num)
-        # list1.extend(equal)
-        # list1.extend(list2)   
-        # return list1
-        equal = 0
-        numLess = 0
-        ans = [0]*len(nums)
-        for num in nums:
-            if num < pivot:
-                numLess += 1
-            elif num == pivot:
-                equal += 1
-        ptrLess = 0
-        ptrEqual = numLess
-        ptrGreater = numLess + equal
-        # print(ptrLess,ptrEqual,ptrGreater)
-        for num in nums:
-            if num < pivot:
-                ans[ptrLess] = num
-                ptrLess += 1
-            elif num == pivot:
-                ans[ptrEqual] = num
-                ptrEqual += 1
-            elif num > pivot:
-                ans[ptrGreater] = num
-                ptrGreater += 1
-        return ans
+        #     if num < pivot:
+        #         numLess += 1
+        #     elif num == pivot:
+        #         equal += 1
+        # ptrLess = 0
+        # ptrEqual = numLess
+        # ptrGreater = numLess + equal
+        # # print(ptrLess,ptrEqual,ptrGreater)
+        # for num in nums:
+        #     if num < pivot:
+        #         ans[ptrLess] = num
+        #         ptrLess += 1
+        #     elif num == pivot:
+        #         ans[ptrEqual] = num
+        #         ptrEqual += 1
+        #     elif num > pivot:
+        #         ans[ptrGreater] = num
+        #         ptrGreater += 1
+        # return ans
