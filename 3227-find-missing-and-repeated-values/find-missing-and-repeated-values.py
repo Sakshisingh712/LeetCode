d35ac08ec5ffabe5fb172287f3aaaab84a82ce01
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-03-06 09:36:55 +0530

    Time: 9 ms (52.44%) | Memory: 18 MB (97.30%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -2,19 +2,17 @@ class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = int(math.pow(len(grid),2))
         dup_dict = {}
-        for i in range(len(grid)):
-            for j in range(len(grid)):
+        for row in grid:
+            for element in row:
                 count = 1
-                # print(j)
-                if dup_dict and grid[i][j] in dup_dict.keys():
+                if dup_dict and element in dup_dict.keys():
                     count += 1
-                dup_dict[grid[i][j]] = count
+                dup_dict[element] = count
+        # print(dup_dict)
         repeated_num = 0
         grid_sum = 0
-        # print(dup_dict)
         for key, val in dup_dict.items():
             grid_sum += key
             if val == 2:
                 repeated_num = key
-        # print((n*(n+1)//2))
         return [repeated_num,(n*(n+1)//2)-grid_sum]
