4602861526398b2ff889437beeec9cccf2e57e17
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-03-12 01:02:57 +0530

    Time: 103 ms (58.11%) | Memory: 17.9 MB (39.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,17 +1,18 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         count = 0
-        hash_table = {'a': 0,'b': 0, 'c': 0}
-        # for right in range(len(s)):
-        #     hash_table[s[right]] = right
-        #     count += max(0, min(hash_table.values())+1)
-        # return count
-        left = 0
+        hash_table = {'a': -1,'b': -1, 'c': -1}
         for right in range(len(s)):
-            hash_table[s[right]] += 1
-            while min(hash_table.values())>0:
-                count += len(s) - right
-                hash_table[s[left]] -= 1
-                left += 1
+            hash_table[s[right]] = right
+            count += max(0, min(hash_table.values())+1)
         del hash_table
         return count
+        # left = 0
+        # for right in range(len(s)):
+        #     hash_table[s[right]] += 1
+        #     while min(hash_table.values())>0:
+        #         count += len(s) - right
+        #         hash_table[s[left]] -= 1
+        #         left += 1
+        # del hash_table
+        # return count
