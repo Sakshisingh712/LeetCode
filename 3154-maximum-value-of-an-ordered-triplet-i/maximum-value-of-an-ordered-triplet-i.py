3d65018d8df34c20c28766176b25cd7365fe2766
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-04-02 20:18:18 +0530

    Time: 3 ms (78.55%) | Memory: 17.7 MB (82.79%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -2,18 +2,17 @@ from typing import List
 
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-            n = len(nums)
-            max_value = 0
-            
-            # Maintain the maximum prefix value nums[i] up to index j
-            max_prefix = [0] * n
-            max_prefix[0] = nums[0]
-            for i in range(1, n):
-                max_prefix[i] = max(max_prefix[i - 1], nums[i])
-            
-            # Traverse to find the maximum triplet value
-            for j in range(1, n - 1):
-                for k in range(j + 1, n):
-                    max_value = max(max_value, (max_prefix[j - 1] - nums[j]) * nums[k])
-            
-            return max_value
+        n = len(nums)
+        if n < 3:
+            return 0
+        
+        max_value = 0
+        max_i = nums[0]  # Maximum value of nums[i] seen so far
+        min_diff = float('-inf')  # Maximum (nums[i] - nums[j]) seen so far
+        
+        for j in range(1, n - 1):
+            min_diff = max(min_diff, max_i - nums[j])
+            max_value = max(max_value, min_diff * nums[j + 1])
+            max_i = max(max_i, nums[j])
+        
+        return max_value
