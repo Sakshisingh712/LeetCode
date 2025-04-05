6e06718024ae285b237a6be1d9f8657222fd3fb7
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-04-05 09:34:04 +0530

    Time: 14 ms (52.62%) | Memory: 17.6 MB (96.02%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,13 +1,13 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        # if len(nums) == 0:
-        #     return 0
-        # else:
-        def xorSum(index, nums, currentSum):
-            if len(nums) == index:
-                return currentSum
-            
-            withCurrent = xorSum(index + 1, nums, currentSum ^ nums[index])
-            withoutCurrent = xorSum(index + 1, nums, currentSum)
-            return withCurrent + withoutCurrent
-        return xorSum(0, nums, 0)
+        if len(nums) == 0:
+            return 0
+        else:
+            def xorSum(index, nums, currentSum):
+                if len(nums) == index:
+                    return currentSum
+                
+                withCurrent = xorSum(index + 1, nums, currentSum ^ nums[index])
+                withoutCurrent = xorSum(index + 1, nums, currentSum)
+                return withCurrent + withoutCurrent
+            return xorSum(0, nums, 0)
