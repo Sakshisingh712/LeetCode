dca1da248487051bb5d0a9c0349711499652f56e
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-05-05 23:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (68.60%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -8,27 +8,41 @@ class Solution:
     #         adjList[v].append(u)
     #     return adjList
 
-    def dfs(self, u, color, colors, adj):
-        from collections import deque
+    # def dfs(self, u, color, colors, adj):
+    #     from collections import deque
 
-        colors[u] = color
-        for v in adj[u]:
-            if colors[v] == -1:
-                if not self.dfs(v, 1 - color, colors, adj):
-                    return False
-            elif colors[v] == color:
-                return False
-        return True
+    #     colors[u] = color
+    #     for v in adj[u]:
+    #         if colors[v] == -1:
+    #             if not self.dfs(v, 1 - color, colors, adj):
+    #                 return False
+    #         elif colors[v] == color:
+    #             return False
+    #     return True
 
 
     def isBipartite(self, graph: List[List[int]]) -> bool:
         V = len(graph)
         colors = [-1] * V
-        # adj_list = self.createAdj(graph, V)
+        # # adj_list = self.createAdj(graph, V)
+        # for i in range(V):
+        #     if colors[i] == -1:
+        #         if not self.dfs(i, 0, colors, graph):
+        #             return False
+        
         for i in range(V):
             if colors[i] == -1:
-                if not self.dfs(i, 0, colors, graph):
-                    return False
+                colors[i] = 0
+                queue = deque([i]) 
+                
+            while queue:
+                u = queue.popleft()
+                for v in graph[u]:
+                    if colors[v] == -1:
+                        colors[v] = 1 - colors[u]
+                        queue.append(v)
+                    elif colors[u] == colors[v]:
+                        return False
         
         return True
 
