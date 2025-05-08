3c7a06535bd61b2e9eb8e64c820ad0cb0b89660f
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-05-08 08:33:46 +0530

    Time: 126 ms (73.75%) | Memory: 18.4 MB (70.08%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -1,22 +1,21 @@
+import heapq
+
 class Solution:
-    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        N, M = len(moveTime), len(moveTime[0])
-        moves = [(0,1),(1,0),(-1,0),(0,-1)]
-        distance = [[math.inf] * M for _ in range(N)]
-        # distance, i, j
-        heap = [(0,0,0)]
+    def minTimeToReach(self, moveTime):
+        m, n = len(moveTime), len(moveTime[0])
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[0][0] = 0
+        heap = [(0, 0, 0)]  # time, row, col
+
         while heap:
-            # pop the smallest distance from heap
-            node = heapq.heappop(heap)
-            if node[0] >= distance[node[1]][node[2]]: continue
-            if node[1] == N-1 and node[2] == M-1: return node[0]
-            # Update distance
-            distance[node[1]][node[2]] = node[0]
-            for move in moves:
-                i,j = node[1] + move[0], node[2] + move[1]
-                if not (0 <= i < N and 0 <= j < M): continue
-                dist = max(node[0], moveTime[i][j]) + 1
-                if dist < distance[i][j]: heapq.heappush(heap, (dist, i, j))
+            time, r, c = heapq.heappop(heap)
+            if (r, c) == (m-1, n-1):
+                return time
+            for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    wait_time = max(time, moveTime[nr][nc]) + 1
+                    if wait_time < dist[nr][nc]:
+                        dist[nr][nc] = wait_time
+                        heapq.heappush(heap, (wait_time, nr, nc))
         return -1
-                    
-        
