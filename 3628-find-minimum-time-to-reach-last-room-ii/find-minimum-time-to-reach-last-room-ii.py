4bd621ebb304ad220c5071be69216bf68e5814d0
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-05-08 08:52:34 +0530

    Time: 1701 ms (81.12%) | Memory: 88.2 MB (85.84%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n, m = len(moveTime), len(moveTime[0])
+        moves = [(0,1),(1,0),(-1,0),(0,-1)]
+        distance = [[math.inf] * m for _ in range(n)]
+        # distance, i, j
+    
+        heap = [(0,0,0)]
+        while heap:
+            time, x, y = heapq.heappop(heap)
+            if x == n-1 and y == m-1:
+                return time
+            for mr, mc in moves:
+                nr, nc = x + mr, y + mc
+                # next_time = moveTime[nr][nc]
+                if 0 <= nr < n and 0 <= nc < m:
+                    cost = (x + y ) % 2 + 1
+                    wait = max(time, moveTime[nr][nc]) + cost
+                    
+                    if wait < distance[nr][nc]:
+                        distance[nr][nc] = wait
+                        heapq.heappush(heap, (wait, nr, nc))
+
+        return -1
