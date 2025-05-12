8c531e0a1e88718d5893782db9099dc4f0b540e1
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-05-12 09:16:46 +0530

    Time: 10 ms (83.35%) | Memory: 17.8 MB (65.57%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,15 +1,39 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        res = set()
-        n =  len(digits)
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    if i == j or j==k or k==i:
-                        continue
-                    num = ((digits[i] * 10) + digits[j]) * 10 + digits[k]
-                    # print(digits[i], digits[j], digits[k])
-                    if num >= 100 and num % 2 == 0:
-                        res.add(num)
-        res = sorted(list(res))
-        return res
+        #-----------Optimised Solution------------------------------
+        count = [0] * 10
+        for d in digits:
+            count[d] += 1
+        # print(count)
+        result = []
+
+        for i in range(100, 1000, 2):  # Only even numbers
+            a = i // 100
+            b = (i // 10) % 10
+            c = i % 10
+
+            count[a] -= 1
+            count[b] -= 1
+            count[c] -= 1
+            # print(count)
+            if count[a] >= 0 and count[b] >= 0 and count[c] >= 0:
+                result.append(i)
+
+            count[a] += 1
+            count[b] += 1
+            count[c] += 1
+            # print(count)
+        return result
+
+        #---------Brute Force-------------------------------------
+        # for i in range(n):
+        #     for j in range(n):
+        #         for k in range(n):
+        #             if i == j or j==k or k==i:
+        #                 continue
+        #             num = ((digits[i] * 10) + digits[j]) * 10 + digits[k]
+        #             # print(digits[i], digits[j], digits[k])
+        #             if num >= 100 and num % 2 == 0:
+        #                 res.add(num)
+        # res = sorted(list(res))
+        # return res
