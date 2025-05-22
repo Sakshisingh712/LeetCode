154354d123f0e9fecbdbeca8898a99149920e1cd
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-05-22 23:04:42 +0530

    Time: 2315 ms (10.37%) | Memory: 33.8 MB (30.96%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,23 +1,22 @@
 class Solution:
-    def subsetSum(self, nums, sums):
-        n = len(nums)
-        dp = [[False for _ in range(sums + 1)] for _ in range(n + 1)]
-        for i in range(n + 1):
-            for j in range(sums + 1):
-                dp[i][0] = True
-
-        for i in range(1, n + 1):
-            for j in range(1, sums + 1):
-                if nums[i - 1] <= j:
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] or dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j]
-        return dp[n][sums]
-
     def canPartition(self, nums: List[int]) -> bool:
         sums = sum(nums)
+        n = len(nums)
         if sums % 2 != 0:
             return False
         else:
-            return self.subsetSum(nums, sums//2)
+            target = sums // 2
+            dp = [[False for _ in range(target + 1)] for _ in range(n + 1)]
+            for i in range(n + 1):
+                for j in range(target + 1):
+                    dp[i][0] = True
+
+            for i in range(1, n + 1):
+                for j in range(1, target + 1):
+                    if nums[i - 1] <= j:
+                        dp[i][j] = dp[i - 1][j - nums[i - 1]] or dp[i - 1][j]
+                    else:
+                        dp[i][j] = dp[i - 1][j]
+            return dp[n][target]
+
 
