994d307c2330315734de0e178301b229b7022633
Sakshisingh712 <71007920+Sakshisingh712@users.noreply.github.com>
2025-06-22 09:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.89%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.py
@@ -8,23 +8,4 @@ class Solution:
             curr += k
         partitioned[-1] += fill  * (k - len(partitioned[-1]))
         return partitioned
-        # p = 0
-        # print(n//k)
-        # for i in range(n//k):
-        #     # print(i)
-        #     temp = ''
-        #     if len(s) < k:
-        #         break
-        #     for j in range(k):
-        #         temp += s[j]
-        #         # print(temp)
-        #     s = s.replace(temp, '')
-        #     partitioned.append(temp)
-        # if len(s) != 0:
-        #     diff = k - len(s)
-        #     temp = s
-        #     for i in range(diff):
-        #         temp += fill
-        #     partitioned.append(temp)
-        # return partitioned
-
+        
